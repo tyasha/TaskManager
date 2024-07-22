6341824a0ada6e267f376f71ce2b6ef3e5d97a81
Yakov Byakov <yakovfox@mail.ru>
2024-07-22 20:55:16 +0300

    1) Создал классы User, Task 2) Создал класс CommonUser 3) реализовал метод, который запускается при запуске Spring

--- src/main/java/org/yakov/Main.java
+++ src/main/java/org/yakov/Main.java
@@ -1,7 +1,28 @@
 package org.yakov;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.yakov.model.CommonUser;
+import org.yakov.util.IdProvider;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            IdProvider idProvider = () -> {
+                int id = (int) (Math.random() * Integer.MAX_VALUE);
+                return String.valueOf(id);
+            };
+
+            CommonUser user = new CommonUser(idProvider);
+            System.out.println("Generated User ID: " + user.getId());
+        };
     }
 }
---
+++ src/main/java/org/yakov/model/CommonUser.java
@@ -0,0 +1,9 @@
+package org.yakov.model;
+
+import org.yakov.util.IdProvider;
+
+public class CommonUser extends User {
+    public CommonUser(IdProvider idProvider) {
+        super(idProvider);
+    }
+}
